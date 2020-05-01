05f1b4e831049313e6966329e4e9c8e445700594
방동현 <gusehdqkd@gmail.com>
2020-05-01 18:45:38 +0900

    무중단 배포 작업중

---
+++ src/main/java/com/study/introduction/web/WebRestController.java
@@ -0,0 +1,22 @@
+package com.study.introduction.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private Environment env;
+
+    @GetMapping("/profile")
+    public String etProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
