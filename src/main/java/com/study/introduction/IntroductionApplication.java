db281313a50679b501083306aa0d9567e7bfa73a
방동현 <gusehdqkd@gmail.com>
2020-05-04 14:33:51 +0900

    설정ì° 역추적중

--- src/main/java/com/study/introduction/IntroductionApplication.java
+++ src/main/java/com/study/introduction/IntroductionApplication.java
@@ -2,19 +2,12 @@ package com.study.introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntroductionApplication {
 
-    public static final String APPLCATION_LOCATIONS = "spring.config.location=" +
-            "classpath:application.yml," +
-            "/app/config/spring-portfolio/real-application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(IntroductionApplication.class)
-                .properties(APPLCATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(IntroductionApplication.class, args);
     }
 
 }
