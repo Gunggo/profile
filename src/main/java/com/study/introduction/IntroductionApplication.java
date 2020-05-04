0dca418bee2e0b88ddac51485c0ac5fac4af18fd
방동현 <gusehdqkd@gmail.com>
2020-05-05 00:26:25 +0900

    revert file

--- src/main/java/com/study/introduction/IntroductionApplication.java
+++ src/main/java/com/study/introduction/IntroductionApplication.java
@@ -1,19 +1,13 @@
 package com.study.introduction;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntroductionApplication {
 
-    public static final String APPLICATION_LOCATION = "spring.config.location=" +
-            "classpath:application.yml" +
-            "/app/config/spring-portfolio/real-application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(IntroductionApplication.class)
-                .properties(APPLICATION_LOCATION)
-                .run(args);
+        SpringApplication.run(IntroductionApplication.class, args);
     }
 
 }
