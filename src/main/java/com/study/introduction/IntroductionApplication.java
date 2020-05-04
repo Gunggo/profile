3e0027d71c3522095568f7c4e43b6c16face0a58
방동현 <gusehdqkd@gmail.com>
2020-05-04 17:32:43 +0900

    properties add

--- src/main/java/com/study/introduction/IntroductionApplication.java
+++ src/main/java/com/study/introduction/IntroductionApplication.java
@@ -2,12 +2,19 @@ package com.study.introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntroductionApplication {
 
+    public static final String APPLICATION_LOCATION = "spring.config.location=" +
+            "classpath:application.yml" +
+            "/app/config/spring-portfolio/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(IntroductionApplication.class, args);
+        new SpringApplicationBuilder(IntroductionApplication.class)
+                .properties(APPLICATION_LOCATION)
+                .run(args);
     }
 
 }
