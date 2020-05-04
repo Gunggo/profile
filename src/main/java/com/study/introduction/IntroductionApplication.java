e01428a99026225572365c5f053773f3b36f3350
방동현 <gusehdqkd@gmail.com>
2020-05-04 23:46:21 +0900

    ELB update

--- src/main/java/com/study/introduction/IntroductionApplication.java
+++ src/main/java/com/study/introduction/IntroductionApplication.java
@@ -1,6 +1,5 @@
 package com.study.introduction;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
